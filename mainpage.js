0cb1817f58b4412b52a33859a8ee974ed3ad7fed
TheRealestDiehl <124006910+TheRealestDiehl@users.noreply.github.com>
2023-12-06 20:52:32 -0500

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,25 @@
+document.getElementById("foods").onclick = function(){
+    window.location.href ="./foods.html"
+}
+
+// function go_food(){
+//     window.location.href = "/foods.html"
+// }
+
+document.getElementById("meat").onclick = function(){
+    window.location.href ="./meat.html"
+}
+
+document.getElementById("vegetables").onclick = function(){
+    window.location.href ="./vegetables.html"
+}
+document.getElementById("fruits").onclick = function(){
+    window.location.href ="./fruits.html"
+}
+
+document.getElementById("checkout").onclick = function(){
+    window.location.href ="./checkout.html"
+}
+document.getElementById("contact").onclick = function(){
+    window.location.href ="./contact.html"
+}
